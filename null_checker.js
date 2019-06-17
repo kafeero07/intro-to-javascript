c33b369c055e469edaed1d89bda66a9838d5810f
kafeero07 <51476794+kafeero07@users.noreply.github.com>
2019-06-17 07:51:51 +0300

    Add files via upload

---
+++ null_checker.js
@@ -0,0 +1,20 @@
+
+
+for (x=1;x<11;x++)
+
+{
+	Prompt ("Input a number");
+	
+/*if the number is less than zero*/	
+		if (x<0)
+			console.log(-1);
+/* in the value is greater than zero*/
+
+else if ( x> 0)
+
+	console.log(1);
+//if the value equals zero*/
+else
+	 console.log(0);
+	}
+
